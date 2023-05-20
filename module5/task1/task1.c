2ad9fc71ff492a2fba629326d59f184789f07d2d
Vealex <mrvealex@gmail.com>
2023-05-20 08:13:14 +0100

    Task1: final

--- module5/task1/task1.c
+++ module5/task1/task1.c
@@ -1,20 +1,20 @@
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lakshmanan");
-MODULE_DESCRIPTION("A Simple Hello World module");
+MODULE_LICENSE("ANY LICENCE HERE");
+MODULE_AUTHOR("Davydov Alexey");
+MODULE_DESCRIPTION("Random text for test there");
 
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    return 0;    // Non-zero return means that the module couldn't be loaded.
+    printk(KERN_INFO "It is my first task in module 5!\n");
+    return 0;
 }
 
 static void __exit hello_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
+    printk(KERN_INFO "See you in the next task.\n");
 }
 
 module_init(hello_init);
